c6b5b29df1508146e05d6863a9c60815b4fad269
Eason-CS <easonh2027@student.cis.edu.hk>
2024-08-30 12:15:49 +0800

    my commit

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello, Please enter your age");
+        Scanner scanner = new Scanner(System.in);
+        int age = Integer.parseInt(scanner.nextLine());
 
+        System.out.println("Please enter your first name");
+        Scanner scannerone = new Scanner(System.in);
+        String firstname = scannerone.nextLine();
 
+        System.out.println("Please enter your favoruite food");
+        Scanner scannertwo = new Scanner(System.in);
+        String favouriteFood = scannertwo.nextLine();
+
+        System.out.println("First name: " + firstname + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
 
     }
 
